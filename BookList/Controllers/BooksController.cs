4c24d1bd2b3745a5186004e795379d4862ce2637
chandu-cs <chandrishas@outlook.com>
2019-02-13 21:29:40 +1300

    Adding Create Book

---
+++ BookList/Controllers/BooksController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookList.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookList.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+        public BooksController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_db.Books.ToList());
+        }
+        //Get : Book/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //Post : Book/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Book book)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Add(book);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+
+        }
+        
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing)
+            {
+                _db.Dispose();
+            }
+        }
+    }
+}
